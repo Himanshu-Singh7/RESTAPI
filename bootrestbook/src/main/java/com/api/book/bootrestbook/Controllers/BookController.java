34998999e372e52393caa4dd17ebd85a71fd10d0
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-11-05 23:37:24 +0530

    Update BookController.java

--- bootrestbook/src/main/java/com/api/book/bootrestbook/Controllers/BookController.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/Controllers/BookController.java
@@ -1,7 +1,6 @@
 package com.api.book.bootrestbook.Controllers;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -79,8 +78,6 @@ public class BookController {
        } catch (Exception e) {
         e.printStackTrace();
          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-       }
-       
-      
+       } 
     }
 }
